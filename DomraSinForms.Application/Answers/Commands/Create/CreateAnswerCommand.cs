497b306315cfac90176c8399a4dd034717668d67
Fores <Fores@DESKTOP-Q9GI0NO>
2023-04-26 12:05:36 +0300

    Answer Commands

---
+++ DomraSinForms.Application/Answers/Commands/Create/CreateAnswerCommand.cs
@@ -0,0 +1,17 @@
+﻿using DomraSinForms.Domen.Models;
+using DomraSinForms.Domen.Models.Answers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomraSinForms.Application.Answers.Commands.Create;
+
+public class CreateAnswerCommand: IRequest<FormAnswers>
+{
+    public List<AnswerBlock> Answers;
+    public string FormId;
+    public string UserId;
+}
---
+++ DomraSinForms.Application/Answers/Commands/Create/CreateAnswerHandler.cs
@@ -0,0 +1,37 @@
+﻿using DomraSinForms.Domen.Models;
+using DomraSinForms.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomraSinForms.Application.Answers.Commands.Create
+{
+    public class CreateAnswerHandler: IRequestHandler<CreateAnswerCommand, FormAnswers>
+    {
+        private readonly ApplicationDbContext _context;
+        public CreateAnswerHandler(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public async Task<FormAnswers> Handle(CreateAnswerCommand request, CancellationToken cancellationToken)
+        {
+            var form = await _context.Forms
+                .Include(f => f.FormAnswers)
+                .FirstOrDefaultAsync(f => f.Id == request.FormId, cancellationToken);
+            if (form == null)
+            {
+                return null;
+            }
+            var answers = new FormAnswers 
+            { Answers = request.Answers };
+            form.FormAnswers.Add(answers);
+            _context.Update(form);
+            await _context.SaveChangesAsync(cancellationToken);
+            return answers;
+        }
+    }
+}
