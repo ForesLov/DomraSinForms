ef309702d2dc41daf536d7ca5c128bfdf464b7af
ForesLov <fores.clerc@gmail.com>
2024-01-29 16:45:59 +0300

    Namespaces changed

--- DomraSin.Domain/Interfaces/Repositories/IQuestionOptionsRepository.cs
+++ DomraSin.Domain/Interfaces/Repositories/IQuestionOptionsRepository.cs
@@ -1,9 +1,4 @@
-﻿using DomraSin.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DomraSin.Domain.Models.FormItems;
 
 namespace DomraSin.Domain.Interfaces.Repositories;
 
