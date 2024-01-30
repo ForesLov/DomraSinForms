54ec134247033ad8256b2442e0667d45ff32a5be
ForesLov <fores.clerc@gmail.com>
2024-01-30 17:27:06 +0300

    Add some classes

---
+++ DomraSin.Persistance/Repositories/AnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using DomraSin.Domain.Interfaces.Repositories;
+using DomraSin.Domain.Models;
+
+namespace DomraSin.Persistence.Repositories
+{
+    public class AnswerRepository : IAnswersRepository
+    {
+        public IQueryable<Answer> GetCollection(string formAnswersId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
